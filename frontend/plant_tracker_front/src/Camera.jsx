4988ef82fdf64432b348c1ca1b5812958e719bec
Justin <o0justinsgamez0o@gmail.com>
2024-12-21 11:13:56 -0500

    fix: Resize the image and ensure RGBA colour space for model on front-end camera

--- frontend/plant_tracker_front/src/Camera.jsx
+++ frontend/plant_tracker_front/src/Camera.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Webcam from "react-webcam";
+import * as tf from '@tensorflow/tfjs';
 
 const videoConstraints = {
     width: 1280,
@@ -9,10 +10,37 @@ const videoConstraints = {
   
 function WebcamCapture() {
     
+  const preprocessImage = async (base64Image, targetHeight, targetWidth) => {
+      const img = new Image();
+      img.src = `data:image/jpeg;base64,${base64Image.replace(/^data:image\/\w+;base64,/, '')}`;
+      
+      return new Promise((resolve) => {
+          img.onload = () => {
+              const canvas = document.createElement('canvas');
+              canvas.width = targetWidth;
+              canvas.height = targetHeight;
+      
+              const ctx = canvas.getContext('2d');
+              ctx.drawImage(img, 0, 0, targetWidth, targetHeight);
+      
+              const imageData = ctx.getImageData(0, 0, targetWidth, targetHeight);
+              const tensor = tf.browser.fromPixels(imageData)
+                  .expandDims(0) // Batch dimension: [1, 128, 128, 3]
+                  .toFloat()
+                  .div(255.0); // Normalize to [0, 1] range
+              
+              resolve(tensor.arraySync()); // Convert tensor to raw JS array
+          };
+      });
+  };
+
     const sendImageToBackend = async (imageSrc) => {
         if (!imageSrc) return;
     
         try {
+          // Preprocess the image; convert to correct format and size of 128x128
+          const preprocessedData = await preprocessImage(imageSrc, 128, 128);
+
           // Send POST request to the backend
           const response = await fetch('/api/v1/models/plant_model:predict', {
             method: 'POST',
@@ -20,9 +48,7 @@ function WebcamCapture() {
               'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-              instances: [{
-                image_bytes: { b64: imageSrc.split(',')[1] } // Send the base64 string without the prefix
-              }]
+              instances: preprocessedData
             }),
           });
     
@@ -31,8 +57,8 @@ function WebcamCapture() {
         } catch (error) {
           console.error("Error sending image to backend:", error);
         }
-      };
-    
+    };
+
     return (<Webcam
         audio={false}
         height={720}
@@ -45,7 +71,6 @@ function WebcamCapture() {
             onClick={() => {
                 const imageSrc = getScreenshot();
                 if (imageSrc) {
-                    console.log("Success");
                     sendImageToBackend(imageSrc);
                 }
             }}
